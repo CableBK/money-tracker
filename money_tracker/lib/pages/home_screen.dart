aaa6a644850d4bf7f860f2bff708f4338c1b0f3f
เคเบิล <cbkcontactmail@gmail.com>
2024-09-14 21:18:28 +0700

    Update home_screen.dart

--- money_tracker/lib/pages/home_screen.dart
+++ money_tracker/lib/pages/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,7 +14,83 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: Column(),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+            alignment: Alignment.bottomCenter,
+            height: 170,
+            decoration: BoxDecoration(color: Colors.blue.shade800),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.person_2_outlined,
+                        size: 50,
+                      ),
+                    ]),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Money Tracker',
+                      style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      'Team folder',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.black.withOpacity(.1)),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.search,
+                          size: 28,
+                          color: Colors.white,
+                        ),
+                        onPressed: () => null,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.black.withOpacity(.1)),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.notifications,
+                          size: 28,
+                          color: Colors.white,
+                        ),
+                        onPressed: () => null,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
       //bottomNavigationBar: ,
     );
   }
